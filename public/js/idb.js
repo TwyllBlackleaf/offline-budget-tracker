edf7d934a88bfafdf1660e4154a17520bb58f6ec
Twyll <twyllblackleaf@gmail.com>
2021-10-08 08:47:01 -0400

    get indexedDB working

---
+++ public/js/idb.js
@@ -0,0 +1,63 @@
+let db;
+const request = indexedDB.open("budget_tracker", 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore("new_item", { autoIncrement: true });
+}
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+        uploadItems();
+    }
+}
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+}
+
+function saveRecord(record) {
+    const transaction = db.transaction(["new_item"], "readwrite");
+
+    const itemObjectStore = transaction.objectStore("new_item");
+
+    itemObjectStore.add(record);
+}
+
+function uploadItems() {
+    const transaction = db.transaction(["new_item"], "readwrite");
+
+    const itemObjectStore = transaction.objectStore("new_item");
+
+    const getAll = itemObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    const transaction = db.transaction(["new_item"], "readwrite");
+                    const itemObjectStore = transaction.objectStore("new_item");
+                    itemObjectStore.clear();
+
+                    alert("All saved items have been submitted.");
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        }
+    }
+}
+
+window.addEventListener("online", uploadItems);
